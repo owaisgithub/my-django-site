52499b44f6613ec73a710dd102c58f5cf95ee91a
owaisgithub <owaismfos@gmail.com>
2020-09-04 13:40:14 +0530

    Add frontend and requirements file

--- mysite_list/my_app/views.py
+++ mysite_list/my_app/views.py
@@ -1,4 +1,13 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
 
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')  #request post return a python get dict
+    stuff_for_frontend = {
+        'search': search,
+    }
+    return render(request, 'my_app/new_search.html', stuff_for_frontend)
